2611dff99f94cbf2d9414037f18563bb61916995
vyomshah14 <vyomshah2021@gmail.com>
2026-01-06 15:58:47 +0530

    Day-1 of JS

---
+++ Day-1/Console.js
@@ -0,0 +1,20 @@
+// First assignment for the pratice 
+console.log("Hello: I'M your coding fun fact guide")
+// Use of variables
+let botname = "CodeBot";
+let botlocation = "Online";
+let favlanguage = "JS";
+// printing the variables
+console.log(`My name is ${botname}, I am located ${botlocation}`)
+console.log(`My favorite programming language is ${favlanguage}`)
+
+let codingFact = `Did you know? '${favlanguage}' was created in just 10 days by Brendan Eich in 1995!`;
+console.log(codingFact);
+// reassigning the variable
+codingFact = `Fun fact: "${favlanguage}" is used by over 97% of developers today!`;
+console.log(codingFact);
+
+codingFact = `Did you know? '${favlanguage}' is the most popular language for web development!`;
+console.log(codingFact);
+// printing the final variable
+console.log(`It was fun sharing these facts with you. Goodbye! - '${botname}' from '${botlocation}'`);
---
+++ Day-1/script.js
@@ -0,0 +1,28 @@
+// Trying to use console.log and info
+console.log("Hello, World!");
+console.info("This is an info message.");
+// Defining variable and printing it 
+let fname = "vyom";
+let lname = 'shah'; 
+let mname = `amit`;
+let hey = `"hello" ${fname} `
+console.log(hey);
+// Defining the undefined variable and null variable
+let time ;
+let date = null;
+console.log(time);
+console.log(date);
+console.log(time , date);
+// Defining the boolean values
+let isLoggedIn = false;
+console.log(isLoggedIn);
+let isStudent = true;
+console.log(isStudent);
+// Defining the object and printing the value of name key
+let student = {
+    name : "vyom",
+    age : 20,
+    isActive : true,
+    subject : {},
+}
+console.log(student.name);
