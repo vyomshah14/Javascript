fae01be069aff06397825b5c778914373f023672
vyomshah14 <vyomshah2021@gmail.com>
2026-01-06 16:52:12 +0530

    Day-2

---
+++ Day-2/Assignment2.js
@@ -0,0 +1,39 @@
+// Challenge 1
+let challenge = "50 days of Javascript"
+// challenge 2
+console.log(challenge);
+//3
+console.log(challenge.length);
+//4,5
+console.log(challenge.toLowerCase(), challenge.toUpperCase());
+//6
+console.group(challenge.substring(0,2));
+//7
+console.log(challenge.substring(3,13));
+//8
+console.log(challenge.includes("String"));
+//9
+console.log(challenge.split(" "));
+//10 
+console.log(challenge.split(" "));
+//11
+Company = "Facebook , Google , Microsoft , Apple , IBM, Oracle"
+console.log(Company.split(","));
+//12
+console.log(challenge.replace("JS", "Python"));
+//13
+console.log(challenge.charAt(15));
+//14
+console.log(challenge.charCodeAt("J"));
+//15
+console.log(challenge.lastIndexOf("a"));
+//16
+sentence = "You cannot end a sentence with because because because is a conjunction"
+console.log(sentence.indexOf("because"));
+//17
+console.log(sentence.lastIndexOf("because"));
+//18
+console.log(challenge.indexOf("a"))
+//19
+console.log(challenge.trim())
+//20
