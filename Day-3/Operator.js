3de930dfedd871b38930dc77f634dbb1f9c8f626
vyomshah14 <vyomshah2021@gmail.com>
2026-01-07 13:06:53 +0530

    Learning the operators in JS

---
+++ Day-3/Operator.js
@@ -0,0 +1,14 @@
+// binary plus and unary plus
+console.log(typeof "10" === 10)
+// ++Var_name // --var_name
+// var_name++ // var_name--
+let counter =1;
+counter++;
+console.log(counter);
+ // ++counter;
+console.log(++counter);
+// --counter;
+console.log(--counter); 
+// counter--
+counter--;
+console.log(counter);
